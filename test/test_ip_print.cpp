8d800ec115e247df9e7c5bdd440e6a35151378fd
Muhammad <abdulvadudovmagomed@mail.ru>
2018-06-02 15:12:07 +0300

    next commit 5

--- test/test_ip_print.cpp
+++ test/test_ip_print.cpp
@@ -8,11 +8,24 @@ using namespace std;
 
 BOOST_AUTO_TEST_SUITE(test_ip_output)
 
+struct cout_redirect {
+    cout_redirect( std::streambuf * new_buffer ) 
+        : old( std::cout.rdbuf( new_buffer ) )
+    { }
+
+    ~cout_redirect( ) {
+        std::cout.rdbuf( old );
+    }
+
+private:
+    std::streambuf * old;
+};
+
 BOOST_AUTO_TEST_CASE(test_ip_type){
 
 	boost::test_tools::output_test_stream output;
-
 	{
+		cout_redirect guard( output.rdbuf( ) );
 		print_ip(char(-1));
 	}
 
