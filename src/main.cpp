615484dc3a10a135355abfec5712b3e6405ad034
Muhammad <abdulvadudovmagomed@mail.ru>
2018-06-04 23:38:42 +0300

    next commit 45

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@ int main(int argc, char *argv[])
     print_ip(int(2130706433));
     print_ip(long(8875824491850138409));
     
-
     vector<int> ip_vec = {14,1,5,6};
     list<short> ip_list = {14,45,5,6};
     string ip_string = "140.1.5.6";
